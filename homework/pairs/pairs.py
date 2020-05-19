cfa17d342c911d546c5daf533607e0e8d5aae4a6
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-05-19 18:19:23 +0300

    docstring added, list_result renamed

--- homework/pairs/pairs.py
+++ homework/pairs/pairs.py
@@ -1,12 +1,14 @@
-def search_pairs(numbers, required_sum):
-    list_result = []
+def search_pairs(numbers: list, required_sum: int) -> list:
+    """Searching for pairs whose sum is equal to a required one."""
+
+    list_of_pairs = []
     all_numbers = set()
     for i in range(0, len(numbers)):
         suitable_pair = required_sum - numbers[i]
         if suitable_pair in all_numbers:
-            list_result.append(f'{suitable_pair} + {numbers[i]}')
+            list_of_pairs.append(f'{suitable_pair} + {numbers[i]}')
         all_numbers.add(numbers[i])
-    return list_result
+    return list_of_pairs
 
 
 if __name__ == '__main__':
