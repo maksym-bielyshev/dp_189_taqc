7614af3aa74b13bfaefb2293c75eb59da4dcf563
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-05-20 21:55:02 +0300

    The draft is changed.

--- homework/threepoint/app.py
+++ homework/threepoint/app.py
@@ -1,28 +1,49 @@
-def task():
-    a = int(input('a'))
-    m = int(input('m'))
-    b = int(input('b'))
-    result = [a,m,b]
-    return result
+import math
+
+def task_time_borders():
+    best_case_estimate = float(input('best-case estimate: '))
+    most_likely_estimate = float(input('most-likely estimate: '))
+    worst_case_estimate = float(input('worst-case estimate: '))
+    return best_case_estimate, most_likely_estimate, worst_case_estimate
+
+
+estimated_tasks = []
+standard_deviation_tasks = []
+
+
+def sd_project():
+    quadra_list = []
+    for i in standard_deviation_tasks:
+        quadra_list.append(i ** 2)
+    print(math.sqrt(sum(quadra_list)))
+
+
+sd_project()
+
+
+def task_estimation(best_case_estimate, most_likely_estimate, worst_case_estimate):
+    return (best_case_estimate+4 * most_likely_estimate + worst_case_estimate)/6
+
+
+def task_standard_deviation(best_case_estimate, most_likely_estimate, worst_case_estimate):
+    return (worst_case_estimate-best_case_estimate)/6
 
 
 def flow():
     while True:
-        print(estimate(*task()))
-        answer = input('Do you want ot analyze another file? y/n: ').lower()
+        task = task_time_borders()
+        estimated_tasks.append(task_estimation(*task))
+        standard_deviation_tasks.append(task_standard_deviation(*task))
+
+        answer = input('Do you want to add another task? y/n: ').lower()
         if answer != 'y':
+
+            print(estimated_tasks)
+            print(sum(estimated_tasks))
+
+            print(standard_deviation_tasks)
+            print(sum(standard_deviation_tasks))
             break
-
-
-        # standard_deviation(a,b)
-
-
-def estimate(a, m, b):
-    return (a+4*m + b)/6
-
-
-def standard_deviation(a, b):
-    (b-a)/6
 
 
 if __name__ == '__main__':
