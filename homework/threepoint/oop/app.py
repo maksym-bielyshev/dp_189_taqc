9540636ffebc6e62ab5065447ef640550143250c
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-05-23 01:16:18 +0300

    The line length is changed to 80. Please enter the commit message for your changes. Lines starting

--- homework/threepoint/oop/app.py
+++ homework/threepoint/oop/app.py
@@ -19,8 +19,10 @@ def console():
         estimated_tasks.append(task.calculate_task_estimation())
         print('E(task) (estimation for each task):', estimated_tasks)
 
-        standard_deviation_tasks.append(task.calculate_task_standard_deviation())
-        print('SD(task) (standard deviation for each task):', standard_deviation_tasks)
+        standard_deviation_tasks.append(
+            task.calculate_task_standard_deviation())
+        print("SD(task) (standard deviation for each task): ",
+              standard_deviation_tasks)
 
         answer = input("Do you want to add another task? y/n: ").lower()
         if answer != "y":
@@ -28,13 +30,16 @@ def console():
             project = Project(estimated_tasks, standard_deviation_tasks)
 
             expected_value_project = project.calculate_expected_value_project()
-            print(f"E(project)(expected value for project): {expected_value_project}")
+            print(f"E(project)(expected value for project): "
+                  f"{expected_value_project}")
 
             standard_error_project = project.calculate_standard_error_project()
-            print(f"SE(project)(standard error for project): {standard_error_project}")
+            print(f"SE(project)(standard error for project): "
+                  f"{standard_error_project}")
 
-            confidence_interval_project = project.calculate_confidence_interval(expected_value_project,
-                                                                                standard_error_project)
+            confidence_interval_project = \
+                Project.calculate_confidence_interval(
+                    expected_value_project, standard_error_project)
             print(confidence_interval_project)
             break
 
--- homework/threepoint/oop/models.py
+++ homework/threepoint/oop/models.py
@@ -5,6 +5,7 @@ class Project:
     def __init__(self, estimated_tasks, standard_deviation_tasks):
         self.estimated_tasks = estimated_tasks
         self.standard_deviation_tasks = standard_deviation_tasks
+        # todo add to atributes
 
     def calculate_expected_value_project(self):
         return sum(self.estimated_tasks)
@@ -16,20 +17,30 @@ class Project:
         return math.sqrt(sum(squared_list))
 
     @staticmethod
-    def calculate_confidence_interval(expected_value_project, standard_error_project):
-        min_confidence_interval = expected_value_project - 2 * standard_error_project
-        max_confidence_interval = expected_value_project + 2 * standard_error_project
-        return f"CI (confidence interval for project): {min_confidence_interval} ... {max_confidence_interval} points"
+    def calculate_confidence_interval(expected_value_project,
+                                      standard_error_project):
+        min_confidence_interval = \
+            expected_value_project - 2 * standard_error_project
+        max_confidence_interval = \
+            expected_value_project + 2 * standard_error_project
+        return f"CI (confidence interval for project): " \
+            f"{min_confidence_interval} ... {max_confidence_interval} points"
 
 
 class Task:
-    def __init__(self, best_case_estimate, most_likely_estimate, worst_case_estimate):
+    def __init__(self, best_case_estimate, most_likely_estimate,
+                 worst_case_estimate, estimation=None):
         self.best_case_estimate = best_case_estimate
         self.most_likely_estimate = most_likely_estimate
         self.worst_case_estimate = worst_case_estimate
+        self.estimation = estimation
+        # todo add to atributes
 
     def calculate_task_estimation(self):
-        return (self.best_case_estimate + 4 * self.most_likely_estimate + self.worst_case_estimate) / 6
+        if self.estimation is None:
+            self.estimation = (self.best_case_estimate +
+                    4 * self.most_likely_estimate +
+                    self.worst_case_estimate) / 6
 
     def calculate_task_standard_deviation(self):
         return (self.worst_case_estimate - self.best_case_estimate)/6
