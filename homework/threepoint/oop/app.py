e61b73a528ce2e9ec9fb535f4fa779284d845049
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-05-24 15:53:29 +0300

    The code is refactored.

--- homework/threepoint/oop/app.py
+++ homework/threepoint/oop/app.py
@@ -2,9 +2,9 @@ from homework.threepoint.oop.models import Project, Task
 
 
 def user_estimation():
-    best_case_estimate = int(input('Best-case estimate: '))
-    most_likely_estimate = int(input('Most-likely estimate: '))
-    worst_case_estimate = int(input('Worst-case estimate: '))
+    best_case_estimate = int(input("Best-case estimate: "))
+    most_likely_estimate = int(input("Most-likely estimate: "))
+    worst_case_estimate = int(input("Worst-case estimate: "))
     return best_case_estimate, most_likely_estimate, worst_case_estimate
 
 
@@ -17,7 +17,7 @@ def console():
         task = Task(*user_input)
 
         estimated_tasks.append(task.calculate_task_estimation())
-        print('E(task) (estimation for each task):', estimated_tasks)
+        print("E(task) (estimation for each task): ", estimated_tasks)
 
         standard_deviation_tasks.append(
             task.calculate_task_standard_deviation())
@@ -26,7 +26,6 @@ def console():
 
         answer = input("Do you want to add another task? y/n: ").lower()
         if answer != "y":
-
             project = Project(estimated_tasks, standard_deviation_tasks)
 
             expected_value_project = project.calculate_expected_value_project()
@@ -38,11 +37,12 @@ def console():
                   f"{standard_error_project}")
 
             confidence_interval_project = \
-                Project.calculate_confidence_interval(
-                    expected_value_project, standard_error_project)
+                Project.calculate_confidence_interval(expected_value_project,
+                                                      standard_error_project)
+
             print(confidence_interval_project)
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     console()
