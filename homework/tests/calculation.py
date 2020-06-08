72a4cd09a953edf56657a5d6347866232b66ff47
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-06-08 11:52:54 +0300

    The testcases are added.

--- homework/tests/calculation.py
+++ homework/tests/calculation.py
@@ -1,6 +1,42 @@
+import unittest
+from unittest import TestCase
+
+
 def add(num1, num2):
     return num1 + num2
 
 
 def subtract(num1, num2):
     return num1 - num2
+
+
+class TestAdd(TestCase):
+    def test_all_arguments_are_greater_than_zero(self):
+        result = add(2, 1)
+        self.assertEqual(result, 3)
+
+    def test_all_arguments_are_less_than_zero(self):
+        result = add(-2, -3)
+        self.assertEqual(result, -5)
+
+    def test_at_least_one_argument_is_zero(self):
+        result = add(4, 0)
+        self.assertEqual(result, 4)
+
+
+class TestSubtract(TestCase):
+    def test_all_arguments_are_greater_than_zero(self):
+        result = subtract(2, 1)
+        self.assertEqual(result, 1)
+
+    def test_all_arguments_are_less_than_zero(self):
+        result = subtract(-2, -3)
+        self.assertEqual(result, 1)
+
+    def test_at_least_one_argument_is_zero(self):
+        result = subtract(4, 0)
+        self.assertEqual(result, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
