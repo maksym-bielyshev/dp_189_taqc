c05db83f2723d71a8e78fb4d67992322e00ae7ae
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-06-08 12:32:31 +0300

    The docstrings and annotations are added.

--- homework/tests/testcases.py
+++ homework/tests/testcases.py
@@ -1,32 +1,62 @@
+"""Test cases for the two functions."""
+
 import unittest
 from unittest import TestCase
 from homework.tests.calculation import add, subtract
 
 
 class TestAdd(TestCase):
+    """Test cases for the function 'add'."""
+
     def test_all_arguments_are_greater_than_zero(self):
+        """Test function with arguments that are greater than zero.
+
+        :return: True or AssertionError
+        """
         result = add(2, 1)
         self.assertEqual(result, 3)
 
     def test_all_arguments_are_less_than_zero(self):
+        """Test function with arguments that are less than zero.
+
+        :return: True or AssertionError
+        """
         result = add(-2, -3)
         self.assertEqual(result, -5)
 
     def test_at_least_one_argument_is_zero(self):
+        """Test function with at least one argument is zero.
+
+        :return: True or AssertionError
+        """
         result = add(4, 0)
         self.assertEqual(result, 4)
 
 
 class TestSubtract(TestCase):
+    """Test cases for the function 'subtract'."""
+
     def test_all_arguments_are_greater_than_zero(self):
+        """Test function with arguments that are greater than zero.
+
+        :return: True or AssertionError
+        """
         result = subtract(2, 1)
         self.assertEqual(result, 1)
 
     def test_all_arguments_are_less_than_zero(self):
+        """Test function with arguments that are less than zero.
+
+        :return: True or AssertionError
+        """
         result = subtract(-2, -3)
         self.assertEqual(result, 1)
 
     def test_at_least_one_argument_is_zero(self):
+        """Test function with at least one argument is zero.
+
+        :return: True or AssertionError
+        """
         result = subtract(4, 0)
         self.assertEqual(result, 4)
 
--- homework/tests/testsuites.py
+++ homework/tests/testsuites.py
@@ -1,8 +1,11 @@
+"""Creating and running the test suite."""
+
 import unittest
 from testcases import TestAdd, TestSubtract
 
 
 def suite():
+    """Create the test suite with the required tests."""
     suite = unittest.TestSuite()
     suite.addTest(TestAdd("test_at_least_one_argument_is_zero"))
     suite.addTest(TestAdd("test_all_arguments_are_less_than_zero"))
