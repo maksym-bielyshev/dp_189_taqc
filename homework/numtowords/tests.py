e5f8443ed3183529e4952e2fcc1761456fd61e43
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-06-09 15:06:14 +0300

    The tests are redesigned.

--- homework/numtowords/tests.py
+++ homework/numtowords/tests.py
@@ -2,28 +2,39 @@
 
 import unittest
 from unittest import TestCase
+from num2words import num2words
 from num_to_words_converter import num2words_fixed_minus_zero_one
 
 
-class NumToWords(TestCase):
-    """Test case for the function 'num2words_fixed_minus_zero_one'."""
+class TestNum2Words(TestCase):
+    """Test case for the functions 'num2words' and fixed version."""
 
     def test_positive(self):
-        """Test function with argument that are greater than zero.
+        """Test function 'num2words' with argument that are greater than zero.
 
         :return: True or AssertionError
         """
-        result = num2words_fixed_minus_zero_one('1')
+        result = num2words('1')
         self.assertEqual(result, 'one')
 
     def test_negative(self):
-        """Test function with argument that are less than zero.
+        """Test function 'num2words' with argument that are less than zero.
 
         :return: True or AssertionError
         """
-        result = num2words_fixed_minus_zero_one('-1')
+        result = num2words('-1')
         self.assertEqual(result, 'minus one')
 
+    def test_negative_from_zero_to_one(self):
+        """Test function with argument that between zero and minus one.
+
+        :return: True or AssertionError
+        """
+        result = num2words('-0.5')
+        self.assertEqual(result, 'minus zero point five')
+
+
+class Num2WordsFixedMinusZeroOne(TestCase):
     def test_negative_from_zero_to_one(self):
         """Test function with argument that between zero and minus one.
 
