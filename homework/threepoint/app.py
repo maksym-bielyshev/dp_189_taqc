7fd5523f7ae23a64e1a529e9bc86cbbb85c0b53b
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-06-14 15:39:43 +0300

    The code is refactored.

--- homework/threepoint/app.py
+++ homework/threepoint/app.py
@@ -1,52 +1,90 @@
 import math
 
 
-def task_time_borders():
-    best_case_estimate = float(input('best-case estimate: '))
-    most_likely_estimate = float(input('most-likely estimate: '))
-    worst_case_estimate = float(input('worst-case estimate: '))
-    return best_case_estimate, most_likely_estimate, worst_case_estimate
+class Project:
+    def __init__(self, estimated_tasks, standard_deviation_tasks):
+        self.estimated_tasks = estimated_tasks
+        self.standard_deviation_tasks = standard_deviation_tasks
+        self.expected_value_project = None
+        self.standard_error_project = None
+
+    def calculate_expected_value_project(self):
+        if self.expected_value_project is None:
+            self.expected_value_project = sum(self.estimated_tasks)
+        return self.expected_value_project
+
+    def calculate_standard_error_project(self):
+        squared_list = []
+        for i in self.standard_deviation_tasks:
+            squared_list.append(pow(i, 2))
+        self.standard_error_project = math.sqrt(sum(squared_list))
+        return self.standard_error_project
+
+    def calculate_confidence_interval(self):
+        min_confidence_interval = \
+            self.expected_value_project - 2 * self.standard_error_project
+
+        max_confidence_interval = \
+            self.expected_value_project + 2 * self.standard_error_project
+
+        return min_confidence_interval, max_confidence_interval
 
 
-estimated_tasks = []
-standard_deviation_tasks = []
+class Task:
+    def __init__(self, best_case_estimate, most_likely_estimate,
+                 worst_case_estimate):
+        self.best_case_estimate = best_case_estimate
+        self.most_likely_estimate = most_likely_estimate
+        self.worst_case_estimate = worst_case_estimate
+        self.estimation = None
+        self.standard_deviation = None
+
+    def calculate_task_estimation(self):
+        if self.estimation is None:
+            self.estimation = (self.best_case_estimate
+                               + self.most_likely_estimate * 4
+                               + self.worst_case_estimate) / 6
+        return self.estimation
+
+    def calculate_task_standard_deviation(self):
+        if self.standard_deviation is None:
+            self.standard_deviation = \
+                (self.worst_case_estimate - self.best_case_estimate) / 6
+        return self.standard_deviation
 
 
-def sd_project():
-    quadra_list = []
-    for i in standard_deviation_tasks:
-        quadra_list.append(pow(i, 2))
-        # quadra_list.append(i ** 2)
-    print(math.sqrt(sum(quadra_list)))
+if __name__ == "__main__":
+    estimated_tasks_values = []
+    standard_deviation_tasks_values = []
 
-
-sd_project()
-
-
-def task_estimation(best_case_estimate, most_likely_estimate, worst_case_estimate):
-    return (best_case_estimate+4 * most_likely_estimate + worst_case_estimate)/6
-
-
-def task_standard_deviation(best_case_estimate, most_likely_estimate, worst_case_estimate):
-    return (worst_case_estimate-best_case_estimate)/6
-
-
-def flow():
     while True:
-        task = task_time_borders()
-        estimated_tasks.append(task_estimation(*task))
-        standard_deviation_tasks.append(task_standard_deviation(*task))
+        def user_estimation():
+            best_case_estimate = int(input("Best-case estimate: "))
+            most_likely_estimate = int(input("Most-likely estimate: "))
+            worst_case_estimate = int(input("Worst-case estimate: "))
+            return \
+                best_case_estimate, most_likely_estimate, worst_case_estimate
 
-        answer = input('Do you want to add another task? y/n: ').lower()
-        if answer != 'y':
+        task = Task(*user_estimation())
 
-            print(estimated_tasks)
-            print(sum(estimated_tasks))
+        task_estimation = task.calculate_task_estimation()
+        estimated_tasks_values.append(task_estimation)
 
-            print(standard_deviation_tasks)
-            print(sum(standard_deviation_tasks))
+        standard_deviation_task = task.calculate_task_standard_deviation()
+        standard_deviation_tasks_values.append(standard_deviation_task)
+
+        answer = input("Do you want to add another task? y/n: ").lower()
+        if answer != "y":
+            project = Project(estimated_tasks_values,
+                              standard_deviation_tasks_values)
+
+            project.calculate_expected_value_project()
+            project.calculate_standard_error_project()
+
+            min_confidence_interval, max_confidence_interval = \
+                project.calculate_confidence_interval()
+
+            print(f"CI (confidence interval for project): "
+                  f"{min_confidence_interval} ... "
+                  f"{max_confidence_interval} points")
             break
-
-
-if __name__ == '__main__':
-    flow()
