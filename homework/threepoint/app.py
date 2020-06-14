4869228f899201d816d34f5089abcefb9f2a4ce0
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-06-14 16:46:54 +0300

    Docstrings and annotations are added, the code is refactored.

--- homework/threepoint/app.py
+++ homework/threepoint/app.py
@@ -6,34 +6,44 @@ import math
 class Project:
     """Project calculation."""
 
-    def __init__(self, estimated_tasks, standard_deviation_tasks):
-        """
+    def __init__(self,
+                 estimated_tasks: list,
+                 standard_deviation_tasks: list) -> None:
+        """Create a project.
 
-        :param estimated_tasks:
-        :param standard_deviation_tasks:
+        :param estimated_tasks: list of estimated tasks
+        :param standard_deviation_tasks: list of standard deviation tasks
         """
         self.estimated_tasks = estimated_tasks
         self.standard_deviation_tasks = standard_deviation_tasks
         self.expected_value_project = None
         self.standard_error_project = None
 
-    def calculate_expected_value_project(self):
-        """Calculate the expected value of the project.
+    def calculate_expected_value_project(self) -> object:
+        """Calculate an expected value of a project.
 
-        :return:
+        :return: expected value of a project
         """
         if self.expected_value_project is None:
             self.expected_value_project = sum(self.estimated_tasks)
         return self.expected_value_project
 
-    def calculate_standard_error_project(self):
+    def calculate_standard_error_project(self) -> object:
+        """Calculate standard error for a project.
+
+        :return: standard error for a project
+        """
         squared_list = []
         for i in self.standard_deviation_tasks:
             squared_list.append(pow(i, 2))
         self.standard_error_project = math.sqrt(sum(squared_list))
         return self.standard_error_project
 
-    def calculate_confidence_interval(self):
+    def calculate_confidence_interval(self) -> object:
+        """Calculate 95% confidence interval.
+
+        :return: 95% confidence interval
+        """
         min_confidence_interval = \
             self.expected_value_project - 2 * self.standard_error_project
 
@@ -44,8 +54,18 @@ class Project:
 
 
 class Task:
-    def __init__(self, best_case_estimate, most_likely_estimate,
-                 worst_case_estimate):
+    """Task calculation."""
+
+    def __init__(self,
+                 best_case_estimate: int,
+                 most_likely_estimate: int,
+                 worst_case_estimate: int) -> None:
+        """Create a task.
+
+        :param best_case_estimate: user best case estimation
+        :param most_likely_estimate: user most likely case estimation
+        :param worst_case_estimate: user worst case estimation
+        """
         self.best_case_estimate = best_case_estimate
         self.most_likely_estimate = most_likely_estimate
         self.worst_case_estimate = worst_case_estimate
@@ -53,6 +73,10 @@ class Task:
         self.standard_deviation = None
 
     def calculate_task_estimation(self):
+        """Calculate a task estimation.
+
+        :return: task estimation
+        """
         if self.estimation is None:
             self.estimation = (self.best_case_estimate
                                + self.most_likely_estimate * 4
@@ -60,6 +84,10 @@ class Task:
         return self.estimation
 
     def calculate_task_standard_deviation(self):
+        """Calculate standard deviation for a task.
+
+        :return: standard deviation for a task
+        """
         if self.standard_deviation is None:
             self.standard_deviation = \
                 (self.worst_case_estimate - self.best_case_estimate) / 6
@@ -106,12 +134,12 @@ if __name__ == "__main__":
             project.calculate_expected_value_project()
             project.calculate_standard_error_project()
 
-            min_confidence_interval, max_confidence_interval = \
+            result_min_confidence_interval, result_max_confidence_interval = \
                 project.calculate_confidence_interval()
 
             print(f"CI (confidence interval for project): "
-                  f"{min_confidence_interval} ... "
-                  f"{max_confidence_interval} points")
+                  f"{result_min_confidence_interval} ... "
+                  f"{result_max_confidence_interval} points")
 
             run_app = input(
                 "Do you want to calculate another project? y/n: ").lower()
