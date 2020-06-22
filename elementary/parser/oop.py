02becb9a97e90d0e1a4ff3a866281b7561bb0437
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-06-22 12:36:25 +0300

    Input is added.

---
+++ elementary/parser/oop.py
@@ -0,0 +1,11 @@
+
+
+
+
+if __name__ == "__main__":
+
+    print(__doc__)
+
+    mode = input("Please select the mode: "
+                 "1. Count the number of line occurrences in a text file. "
+                 "2. Replace the string with another one in a text file. ")
