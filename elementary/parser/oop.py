3f9b8703cf09d9f350d5956b9af2ba01006fafcb
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-06-22 19:06:18 +0300

    Task in OOP and test file are added.

--- elementary/parser/oop.py
+++ elementary/parser/oop.py
@@ -1,11 +1,132 @@
+"""'File Parser' app can count the number of specific lines or replace them."""
+
+import os
+from abc import ABC, abstractmethod
 
 
+class Option(ABC):
+    """Abstract class for creating options."""
+
+    @abstractmethod
+    def process_file(self,
+                     file: str,
+                     string_for_search: str,
+                     string_for_replace: str) -> None:
+        """Process a file with the specified parameters.
+
+        :param file: path to a file
+        :param string_for_search: the string to find in the file
+        :param string_for_replace: replacement string
+        :return: None
+        """
+        pass
+
+    @staticmethod
+    def pick_option(option: str):
+        """Pick an option.
+
+        :param option: user provided option
+        :return: class with option or False
+        """
+        if option == "1":
+            return OccurrencesCounter()
+        if option == "2":
+            return Replacer()
+        else:
+            return False
+
+
+class OccurrencesCounter(Option):
+    """Occurrences counter class."""
+
+    def process_file(self,
+                     file: str,
+                     string_for_search: str,
+                     string_for_replace: str) -> str:
+        """Search for the number of line occurrences in the file.
+
+        :param file: path to a file
+        :param string_for_search: the string to find in the file
+        :param string_for_replace: replacement string
+        :return: string with result
+        """
+        with open(file) as current_file:
+            occurrences_number = 0
+            for line in current_file:
+                if line.strip() == line_for_search:
+                    occurrences_number += 1
+            return f"Number of line occurrences: {occurrences_number}"
+
+
+class Replacer(Option):
+    """Replacer class."""
+
+    def process_file(self,
+                     file: str,
+                     string_for_search: str,
+                     string_for_replace: str) -> str:
+        """Search for the number of line occurrences in the file.
+
+        :param file: path to a file
+        :param string_for_search: the string to find in the file
+        :param string_for_replace: replacement string
+        :return: string with result
+        """
+        with open(file) as provided_file, \
+                open(f"replaced_{file}", "w") as replaced_file:
+
+            for line in provided_file:
+                if line.strip() == line_for_search:
+                    replaced_file.write(
+                        line.replace(line, f"{line_for_replace}\n"))
+                else:
+                    replaced_file.write(line)
+
+        return f"Your new file is ready! Name of the file: (replaced_{file})"
 
 
 if __name__ == "__main__":
 
     print(__doc__)
 
-    mode = input("Please select the mode: "
-                 "1. Count the number of line occurrences in a text file. "
-                 "2. Replace the string with another one in a text file. ")
+    run_app = "y"
+
+    while run_app == "y" or run_app == "yes":
+
+        user_mode = input("Please select the mode (enter the number): \n"
+                          "1. Count the number of line occurrences. \n"
+                          "2. Replace the string with another one. \n")
+
+        user_option = Option.pick_option(user_mode)
+
+        if user_option:
+
+            file = input("Please enter the path to the file: ")
+
+            if os.path.exists(file) and file.endswith(".txt"):
+
+                line_for_search = \
+                    input("Please enter the line you want to find: ")
+
+                if user_mode == "2":
+                    line_for_replace = \
+                        input("Please enter the line you want to replace: ")
+                else:
+                    line_for_replace = None
+
+                result = user_option.process_file(
+                    file,
+                    line_for_search,
+                    line_for_replace
+                )
+
+                print(result)
+
+                run_app = input("Do you want to continue? "
+                                "If yes, enter 'y' or 'yes': ")
+
+            else:
+                print("No such file!")
+
+        else:
+            print("No such option!")
